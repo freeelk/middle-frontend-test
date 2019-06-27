6581692d32a8a8b12ca41184784caa8ef964b6f9
Volodymyr <freeelk@gmail.com>
2019-06-27 13:38:08 +0300

    Some design improvements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {NotFoundComponent} from './not-found/not-found.component';
 import {HomeModule} from './home/home.module';
 import {ProductsModule} from './products/products.module';
 
 const routes: Routes = [
-  // { path: '', loadChildren: '/app/home/home.module#HomeModule'},
-  { path: '', loadChildren: () => HomeModule},
+
+  /**
+   *  Не смог заставить это работать.
+   */
+  /*  { path: '', loadChildren: 'app/home/home.module#HomeModule'},
+    { path: 'home', loadChildren: 'app/home/home.module#HomeModule'},
+    { path: 'products', loadChildren: 'app/products/products.module#ProductsModule'},*/
+
+
+{ path: '', loadChildren: () => HomeModule},
   { path: 'home', loadChildren: () => HomeModule},
-  // { path: 'products', loadChildren: '/app/products/products.module#ProductsModule'},
   { path: 'products', loadChildren: () => ProductsModule},
   { path: '**', component: NotFoundComponent }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const appRouter: ModuleWithProviders = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {appRouter} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import {BooksService} from './books.service';
 import {HttpClientModule} from '@angular/common/http';
-import { ColumnsTextComponent } from './shared/columns-text/columns-text.component';
-import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +16,7 @@ import { AlertComponent } from './shared/alert/alert.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    appRouter,
     HttpClientModule
   ],
   providers: [BooksService],
